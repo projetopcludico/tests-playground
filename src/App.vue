2ad1ab7b342af68d76b3698f2a6c450d155ca113
AndreRicardoSc <andrenarutto7@gmail.com>
2025-10-02 15:14:26 -0300

    Correct dev

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,9 @@
 <script setup>
-    import sortObjects from './tools/sort';
-
-    const obj = [
-        {
-            id: 1,
-            name: 'circle'
-        },
-        {
-            id: 2,
-            name: 'rectangle'
-        },
-        {
-            id: 3,
-            name: 'pentagon'
-        },
-        {
-            id: 4,
-            name: 'triangle'
-        },
-    ]
+    
 </script>
 
 <template>
-    <p v-for="item of sortObjects(2, obj)">
-        {{ item.name }}
-    </p>
+    
 </template>
 
 <style scoped>
