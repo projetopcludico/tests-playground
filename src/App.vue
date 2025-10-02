1d1b1743161672daaabeba3991b2bc8177e96993
AndreRicardoSc <andrenarutto7@gmail.com>
2025-10-02 14:57:42 -0300

    Teste de sorteio de itens

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script setup>
+    import sort from './tools/sort';
+
+    const obj = [
+        {
+            id: 1,
+            name: 'circle'
+        },
+        {
+            id: 2,
+            name: 'rectangle'
+        },
+        {
+            id: 3,
+            name: 'pentagon'
+        },
+        {
+            id: 4,
+            name: 'triangle'
+        },
+    ]
+</script>
+
+<template>
+    <p v-for="item of sort(2, obj)">
+        {{ item.name }}
+    </p>
+</template>
+
+<style scoped>
+
+</style>
