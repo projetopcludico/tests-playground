6d70025782910f383767972ca432b403c45c50af
AndreRicardoSc <andrenarutto7@gmail.com>
2025-10-02 15:06:33 -0300

    Teste de sorteio de itens

---
+++ src/tools/sort.js
@@ -0,0 +1,21 @@
+function sortObjects(quantidade, arrayObj){
+    if(quantidade > arrayObj.length){
+        throw new Error('Quantidade não pode ser maior do que o tamanho do array');
+    }
+
+    const selecionados = [];
+    while(selecionados.length < quantidade) {
+        const randomIndex = Math.floor(Math.random() * arrayObj.length);
+        const drawn = arrayObj[randomIndex];
+
+        if(selecionados.find(obj => obj.id == drawn.id)){
+            continue;
+        }
+        
+        selecionados.push(drawn)
+    }
+
+    return selecionados;
+}
+
+export default sortObjects;
