727381eb4efb02831e8cb37f38e78a1507c28ab2
AndreRicardoSc <andrenarutto7@gmail.com>
2025-10-16 16:30:51 -0300

    Rota da HomeTest

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,12 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/sort/:quantidade',
+      path:'/',
+      name: 'homeTest',
+      component: () => import('@/views/HomeTestPage.vue')
+    },
+    {
+      path: '/sort/:numberForms/:size/:discovers',
       name: 'sortTest',
       component: () => import('@/views/SortTestPage.vue'),
       props: true
