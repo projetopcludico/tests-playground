dbdf6e4b4d82561a2e57654fda812f19aceb0c31
AndreRicardoSc <andrenarutto7@gmail.com>
2025-11-04 07:52:32 -0300

    Removendo alguns comentários

--- src/stores/global.js
+++ src/stores/global.js
@@ -1,11 +1,8 @@
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
-import { useRouter } from 'vue-router'
 
 export const useAplicationStore = defineStore('aplicationStore', () => {
-  const router = useRouter()
 
-  //teste do que seria uma campanha das formas
   const aplication = ref({
     themes: {
       forms: {
@@ -19,17 +16,17 @@ export const useAplicationStore = defineStore('aplicationStore', () => {
         difficulties: {
           easy: {
             objective: 10,
-            timeLimit: 30000, //milissegundos
+            timeLimit: 30000,
             params: { numberForms: 2, size: 6, discovers: 1 },
           },
           medim: {
             objective: 7,
-            timeLimit: 30000, //milissegundos
+            timeLimit: 30000,
             params: { numberForms: 2, size: 12, discovers: 2 },
           },
           hard: {
             objective: 4,
-            timeLimit: 30000, //milissegundos
+            timeLimit: 30000,
             params: { numberForms: 3, size: 18, discovers: 3 },
           },
         },
