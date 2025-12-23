ff0e92b94c3104502190c641d8b79f8693c606d6
AndreRicardoSc <andrenarutto7@gmail.com>
2025-12-22 22:15:49 -0300

    Alterando os sons para seram mais distinguíveis

--- src/stores/aplication.js
+++ src/stores/aplication.js
@@ -11,13 +11,12 @@ export const useAplicationStore = defineStore('aplicationStore', () => {
         required: 5,
         countResponses: 5,
         objects: [
-            {id: 1, path: '/sounds/C4.mp3', name: 'Dó', color: 'blue'},
-            {id: 2, path: '/sounds/D4.mp3', name: 'Ré', color: 'green'},
-            {id: 3, path: '/sounds/E4.mp3', name: 'Mi', color: 'red'},
-            {id: 4, path: '/sounds/F4.mp3', name: 'Fá', color: 'gray'},
-            {id: 5, path: '/sounds/G4.mp3', name: 'Sol', color: 'brown'},
-            {id: 6, path: '/sounds/A4.mp3', name: 'Lá', color: 'yellow'},
-            {id: 7, path: '/sounds/B4.mp3', name: 'Si', color: 'purple'},
+            {id: 1, path: '/sounds/grass.mp3', name: 'som de grama', color: 'blue'},
+            {id: 2, path: '/sounds/metal.mp3', name: 'som de metal', color: 'green'},
+            {id: 3, path: '/sounds/plastic.mp3', name: 'som de plástico', color: 'red'},
+            {id: 4, path: '/sounds/water.mp3', name: 'som de água', color: 'gray'},
+            {id: 5, path: '/sounds/wind.mp3', name: 'som de vento', color: 'brown'},
+            {id: 6, path: '/sounds/wood.mp3', name: 'som de madeira', color: 'yellow'},
         ],
         difficulties: {
             easy:{
