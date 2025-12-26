45a8d3232e199e16a20e4a1f3d03693b60527930
AndreRicardoSc <andrenarutto7@gmail.com>
2025-12-26 14:12:12 -0300

    Acionando feedbacks sonoros

--- src/components/SoundResponse.vue
+++ src/components/SoundResponse.vue
@@ -12,54 +12,59 @@
         params: Object
     })
 
+    const correctSound = new Audio('/sounds/warns/correct.mp3');
+    const nextStageSound = new Audio('/sounds/warns/nextStage.mp3');
+    const errorSound = new Audio('/sounds/warns/error.mp3');
+
     function respond(choiceId, choiceObject, path) {
-    emits('playAudio', path)
-    const index = sequenceStore.selectedIndex
+        emits('playAudio', path)
+        const index = sequenceStore.selectedIndex
 
-    if (index === null || index === undefined) return;
+        if (index === null || index === undefined) return;
 
-    const correctIdForPosition = sequenceStore.correctResponses[index]
+        const correctIdForPosition = sequenceStore.correctResponses[index]
 
-    if (correctIdForPosition === undefined) {
-        console.log('Esta posição não tem resposta registrada:', index)
-        sequenceStore.selectedIndex = null
-        return
-    }
-
-    if (correctIdForPosition == choiceId) {
-        sequenceStore.responses.push({ index, id: choiceId })
-        sequenceStore.revealChoice(index, choiceObject)
-        sequenceStore.selectedIndex = null
-    } else {
-        console.log(
-        `Reposta errada para a posição ${index}, esperado: ${correctIdForPosition}, recebido ${choiceId}`,
-        )
-        sequenceStore.selectedIndex = null
-        return
-    }
-
-    const expectedCount = sequenceStore.correctResponses.filter((id) => id !== null).length
-    if (sequenceStore.responses.length === expectedCount) {
-        if (verifyResponse(sequenceStore.responses, sequenceStore.correctResponses)) {
-            aplicationStore.aplication.themes.sounds.countResponses++
-            sequenceStore.mountSequence(
-                props.params.numberSounds, 
-                props.params.size, 
-                props.params.discover, 
-                aplicationStore.aplication.themes.sounds.objects
-            );
-            sequenceStore.responses = []
-            console.log('Acertou!')
-        } else {
-            sequenceStore.mountSequence(
-                props.params.numberSounds, 
-                props.params.size, 
-                props.params.discover, 
-                aplicationStore.aplication.themes.sounds.objects
-            );
-            console.log('Errou!')
+        if (correctIdForPosition === undefined) {
+            console.log('Esta posição não tem resposta registrada:', index)
+            sequenceStore.selectedIndex = null
+            return
         }
-    }
+
+        if (correctIdForPosition == choiceId) {
+            sequenceStore.responses.push({ index, id: choiceId })
+            sequenceStore.revealChoice(index, choiceObject)
+            sequenceStore.selectedIndex = null
+            correctSound.play();
+        } else {
+            console.log(
+            `Reposta errada para a posição ${index}, esperado: ${correctIdForPosition}, recebido ${choiceId}`,
+            )
+            sequenceStore.selectedIndex = null
+            errorSound.play();
+            return
+        }
+
+        const expectedCount = sequenceStore.correctResponses.filter((id) => id !== null).length
+        if (sequenceStore.responses.length === expectedCount) {
+            if (verifyResponse(sequenceStore.responses, sequenceStore.correctResponses)) {
+                aplicationStore.aplication.themes.sounds.countResponses++
+                sequenceStore.mountSequence(
+                    props.params.numberSounds, 
+                    props.params.size, 
+                    props.params.discover, 
+                    aplicationStore.aplication.themes.sounds.objects
+                );
+                sequenceStore.responses = []
+                nextStageSound.play();
+            } else {
+                sequenceStore.mountSequence(
+                    props.params.numberSounds, 
+                    props.params.size, 
+                    props.params.discover, 
+                    aplicationStore.aplication.themes.sounds.objects
+                );
+            }
+        }
     }
 </script>
 
