8af571c70a7285150a4a9c7d1f8058b942b86c9b
AndreRicardoSc <andrenarutto7@gmail.com>
2026-01-12 13:27:46 -0300

    Criação dos cards que vão exibir a sequência de números

---
+++ src/components/numbers/SequenceCardComp.vue
@@ -0,0 +1,13 @@
+<script setup>
+    const props = defineProps({
+        number: {
+            type: Number,
+            required: true,
+        }
+    });
+</script>
+<template>
+  <button class="h-20 w-20 bg-pink-500 text-white text-xl">
+    {{ props.number }}
+  </button>
+</template>
--- src/views/NumbersPage.vue
+++ src/views/NumbersPage.vue
@@ -1,6 +1,12 @@
-<script setup></script>
+<script setup>
+  import SequenceCardComp from '../components/numbers/SequenceCardComp.vue'
+  const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+</script>
 <template>
   <main class="flex flex-col items-center">
     <h1 class="text-4xl">Numbers Page</h1>
+    <div class="flex flex-wrap gap-5">
+      <SequenceCardComp v-for="number of numbers" :number="number"/>
+    </div>
   </main>
 </template>
