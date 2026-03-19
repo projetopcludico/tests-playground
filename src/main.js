ba2288b158b0a91498b63a5c76e1261e8d7de810
AndreRicardoSc <andrenarutto7@gmail.com>
2026-03-19 15:00:25 -0300

    FEAT: Configuring routes

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import '@mdi/font/css/materialdesignicons.css';
+import '@/assets/main.css'
 
 const app = createApp(App)
 
---
+++ src/views/HomeView.vue
@@ -0,0 +1,6 @@
+<script setup>
+
+</script>
+<template>
+    <div class=""></div>
+</template>
